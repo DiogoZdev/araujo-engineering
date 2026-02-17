d8fdff648e62fa370c5e0eda0f17bb3bfd01a486
Diogo Lara <diogo.lara.dev@gmail.com>
2026-02-17 17:14:43 -0300

    feat: update style according to design file

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,7 +3,8 @@ import { Mail, Phone } from "lucide-react";
 export function Home() {
   return (
     <>
-      <div className="h-[100vh]">
+      <div className="h-[100vh] relative">
+        <img src="logo.svg" alt="M. A. Logo" className="absolute w-[50px] m-8" />
         <div
           className="h-[70%] flex flex-col justify-center items-center bg-base-100"
           style={{
@@ -13,19 +14,19 @@ export function Home() {
           }}
         >
           <div className="">
-            <h1 className="text-[41px] md:text-[82px] font-bold uppercase leading-none text-green-600">
+            <h1 className="text-[41px] md:text-[82px] font-bold uppercase leading-none text-[var(--primary-900)]">
               Medeiros
             </h1>
-            <h1 className="text-[52px] md:text-[104px] font-bold uppercase leading-none text-green-600">
+            <h1 className="text-[52px] md:text-[110px] font-bold uppercase leading-none text-[var(--primary-900)]">
               Araujo
             </h1>
-            <h1 className="text-[32px] md:text-[65px] font-bold uppercase leading-none text-green-400">
+            <h1 className="text-[32px] md:text-[65px] font-bold uppercase leading-none text-[var(--primary-700)]">
               engenharia
             </h1>
           </div>
         </div>
         <footer className="h-[30%] grid md:grid-cols-2">
-          <div className="grid place-content-center bg-green-600 gap-4 p-8">
+          <div className="grid place-content-center bg-[var(--primary-900)] gap-4 p-8">
             <a href="mailto:contato@medeirosaraujo.com.br">
               <div className="flex gap-4 text-sm md:text-xl font-semibold text-base-100">
                 <Mail />
@@ -34,12 +35,12 @@ export function Home() {
             </a>
             <a href="https://wa.me/554792713621">
               <div className="flex gap-4 text-sm md:text-xl font-semibold text-base-100">
-                <Phone /> 47 99271-3621
+                <Phone /> (47) 99271-3621
               </div>
             </a>
           </div>
           <div className="grid grid-cols-2">
-            <div className="bg-green-500 grid place-content-center p-16">
+            <div className="bg-[var(--primary-700)] grid place-content-center p-16">
               <span className="block font-semibold text-sm md:text-xl text-base-100">
                 Projetos estruturais e complementares
               </span>
@@ -48,7 +49,7 @@ export function Home() {
               style={{
                 background: "url('/buildingx4.jpg')",
                 backgroundSize: "cover",
-                backgroundPositionY: -50,
+                backgroundPositionY: -60,
               }}
               className="bg-green-400"
             ></div>
